bb9722522080ffaf9eee54c74e9f7f180b57a5e6
yolandadwinurjanah <yolandadwinurjanah130@gmail.com>
2023-03-30 06:49:00 -0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya yolanda dwi nur janah 
+        saya tinggal di daerah cikarang 
+        olah raga yang saya sukai adalah Bulutangkis";
+    }
+}
